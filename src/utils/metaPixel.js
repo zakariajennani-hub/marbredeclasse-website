f0be78c90e4bbd991fc54f1818621c8a01c20888
zakariajennani-hub <zakariajennani@gmail.com>
2026-06-11 15:48:42 +0200

    Fix Meta Pixel without package

--- src/utils/metaPixel.js
+++ src/utils/metaPixel.js
@@ -1,33 +1,64 @@
-import ReactPixel from "react-facebook-pixel";
-
-const pixelId = "864493949510284";
+const PIXEL_ID = "864493949510284";
 
 export const initPixel = () => {
-  ReactPixel.init(pixelId);
-  ReactPixel.pageView();
+  if (typeof window === "undefined") return;
+
+  if (!window.fbq) {
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = true;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = true;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(
+      window,
+      document,
+      "script",
+      "https://connect.facebook.net/en_US/fbevents.js"
+    );
+
+    window.fbq("init", PIXEL_ID);
+  }
+
+  window.fbq("track", "PageView");
 };
 
 export const trackPageView = () => {
-  ReactPixel.pageView();
+  if (window.fbq) window.fbq("track", "PageView");
 };
 
 export const trackLead = () => {
-  ReactPixel.track("Lead");
+  if (window.fbq) window.fbq("track", "Lead");
 };
 
-export const trackViewContent = (productName) => {
-  ReactPixel.track("ViewContent", {
-    content_name: productName,
-  });
+export const trackViewContent = (productName = "Marbre De Classe Product") => {
+  if (window.fbq) {
+    window.fbq("track", "ViewContent", {
+      content_name: productName,
+    });
+  }
 };
 
 export const trackWhatsAppClick = () => {
-  ReactPixel.trackCustom("WhatsAppClick");
+  if (window.fbq) window.fbq("trackCustom", "WhatsAppClick");
 };
 
-export const trackAddToCart = (value) => {
-  ReactPixel.track("AddToCart", {
-    value,
-    currency: "MAD",
-  });
+export const trackAddToCart = (value = 0) => {
+  if (window.fbq) {
+    window.fbq("track", "AddToCart", {
+      value,
+      currency: "MAD",
+    });
+  }
 };
