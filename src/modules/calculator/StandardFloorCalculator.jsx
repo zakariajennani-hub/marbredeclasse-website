061cefa2c8ffb82570925ae74169ae2712b892ee
zakariajennani-hub <zakariajennani@gmail.com>
2026-06-11 21:27:21 +0200

    Fix meta pixel import path

--- src/modules/calculator/StandardFloorCalculator.jsx
+++ src/modules/calculator/StandardFloorCalculator.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import {
   trackAddToCart,
   trackInitiateCheckout,
-} from "../utils/metaPixel";
+} from "../../utils/metaPixel";
 
 import deliveryZones from "../floor/deliveryZones";
 import floorServicesData from "../floor/floorServicesData";
